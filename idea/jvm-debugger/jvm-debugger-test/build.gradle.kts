02a6515fecf94a4ab991a51bddcf0591ee7b37da
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2019-06-26 22:41:56 +0900

    Refactor debugger tests
    1. Move tests to their own module
    2. Avoid sharing the 'tinyApp' project between tests
    3. Clean up option directive handling

---
+++ idea/jvm-debugger/jvm-debugger-test/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    kotlin("jvm")
+    id("jps-compatible")
+}
+
+dependencies {
+    testCompileOnly(intellijDep())
+
+    testCompile(project(":idea:jvm-debugger:jvm-debugger-core"))
+    testCompile(project(":idea:jvm-debugger:jvm-debugger-evaluation"))
+    testCompile(project(":idea:jvm-debugger:jvm-debugger-sequence"))
+    testCompile(project(":compiler:backend"))
+    testCompile(files("${System.getProperty("java.home")}/../lib/tools.jar"))
+    testCompile(project(":kotlin-test:kotlin-test-junit"))
+    testCompile(projectTests(":compiler:tests-common"))
+    testCompile(projectTests(":idea:idea-test-framework")) { isTransitive = false }
+    testCompile(commonDep("junit:junit"))
+
+    testCompile(intellijPluginDep("stream-debugger"))
+
+    Platform[192].orHigher {
+        testCompileOnly(intellijPluginDep("java"))
+    }
+
+    testRuntime(project(":nj2k:nj2k-services")) { isTransitive = false }
+    testRuntime(project(":idea:idea-jvm"))
+    testRuntime(project(":idea:idea-native")) { isTransitive = false }
+    testRuntime(project(":idea:idea-gradle-native")) { isTransitive = false }
+    testRuntime(project(":kotlin-native:kotlin-native-library-reader")) { isTransitive = false }
+    testRuntime(project(":kotlin-native:kotlin-native-utils")) { isTransitive = false }
+
+    testRuntime(project(":kotlin-reflect"))
+    testRuntime(project(":sam-with-receiver-ide-plugin"))
+    testRuntime(project(":allopen-ide-plugin"))
+    testRuntime(project(":noarg-ide-plugin"))
+    testRuntime(project(":kotlin-scripting-idea"))
+    testRuntime(project(":kotlinx-serialization-ide-plugin"))
+
+    testRuntime(intellijDep())
+    testRuntime(intellijRuntimeAnnotations())
+}
+
+sourceSets {
+    "main" { none() }
+    "test" { projectDefault() }
+}
+
+testsJar()
